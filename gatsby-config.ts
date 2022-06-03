074d4dfe6ee427151ba1679f55302327952d2002
Kol Crooks <kolcrooks10@gmail.com>
2022-06-03 09:11:38 -0400

    deploy scripts

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,7 +3,7 @@ import type { GatsbyConfig, CreateWebpackConfigArgs } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `Kol Crooks`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://kolcrooks.com`
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
